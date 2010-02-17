7f6a654f80a77483f355e8864ce9f5a52621096f
coxhead.john <coxhead.john@e0cf6664-1561-11df-8e3c-378b6dd812f8>
2010-02-17 13:08:25 +0000

    test update

--- trunk/src/ScheduMail.Core/FakeRepositories/FakeBookRepository.cs
+++ trunk/src/ScheduMail.Core/FakeRepositories/FakeBookRepository.cs
@@ -8,15 +8,13 @@ using ScheduMail.Core.RepositoryInterfaces;
 namespace ScheduMail.Core.FakeRepositories
 {
     public class FakeBookRepository : IEBookRepository
-    {
-        private string dummy;
+    {       
         public IEnumerable<Book> ListEbook()
         {
             List<Book> books = new List<Book>();
             books.Add(new Book { ID = 1, Author = "Steven Sanderson", Name = "Pro ASP.NET MVC framewok" });
             books.Add(new Book { ID = 2, Author = "Stphan Waler", Name = "ASP.NET MVC framework Unleashed" });
-            books.Add(new Book { ID = 3, Author = "Scott Guthrie", Name = "ASP.NET MVC 1.0" });
-            dummy = "Test this ..";
+            books.Add(new Book { ID = 3, Author = "Scott Guthrie", Name = "ASP.NET MVC 1.0" });           
             return books;
         }
     }
