ef40eb4f7a256608581eb6a08d71d0c0354a4608
coxhead.john <coxhead.john@e0cf6664-1561-11df-8e3c-378b6dd812f8>
2010-02-17 13:03:05 +0000

    Set up of skeleton projects

---
+++ src/ScheduMail.Core/FakeRepositories/FakeUserRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScheduMail.Core.RepositoryInterfaces;
+using ScheduMail.DBModel;
+
+namespace ScheduMail.Core.FakeRepositories
+{
+    public class FakeUserRepository : IUserRepository
+    {
+        #region ICrudRepository<User,int> Members
+
+        private static IQueryable<User> FakeUsers = new List<User>
+        {
+            new User { Id = 0, FullName = "John Coxhead" },
+            new User { Id = 1, FullName = "Jane Mansfield" },
+            new User { Id = 2, FullName = "Jock Stein" },
+            new User { Id = 3, FullName = "Lois Saha" }
+
+        }.AsQueryable();
+
+        public IQueryable<ScheduMail.DBModel.User> List
+        {
+            get { return FakeUsers; }
+        }
+
+        public ScheduMail.DBModel.User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ScheduMail.DBModel.User Save(ScheduMail.DBModel.User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ScheduMail.DBModel.User Update(ScheduMail.DBModel.User entity, ScheduMail.DBModel.User originalEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ScheduMail.DBModel.User Update(ScheduMail.DBModel.User entity, ScheduMail.DBModel.User originalEntity, bool attach)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(ScheduMail.DBModel.User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ src/ScheduMail.WebMvc2/WindsorControllerFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
+using Castle.Core;
+using Castle.Core.Resource;
+using Castle.Windsor;
+using Castle.Windsor.Configuration.Interpreters;
+
+public class WindsorControllerFactory : DefaultControllerFactory
+{
+    WindsorContainer container;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WindsorControllerFactory"/> class.
+    /// The constructor
+    /// 1. Sets up a new Ioc container.
+    /// 2. Registers all components specified in the web.config.
+    /// 3. Registers all controller types as components.
+    /// </summary>
+    public WindsorControllerFactory()
+    {
+        container = new WindsorContainer
+        (
+            new XmlInterpreter(new ConfigResource("castle"))
+        );
+
+        var controllerTypes = from t in Assembly.GetExecutingAssembly().GetTypes()
+                              where typeof(IController).IsAssignableFrom(t)
+                              select t;
+        foreach (Type t in controllerTypes)
+        {
+            container.AddComponentWithLifestyle(t.FullName, t,
+                LifestyleType.Transient);
+        }
+    }
+
+    protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+    {
+        // return base.GetControllerInstance(requestContext, controllerType);
+        return (IController)container.Resolve(controllerType);
+    }  
+}
