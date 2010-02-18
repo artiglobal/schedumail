897fc5ac984bc7d2589733a2ed96e06409ec8afa
coxhead.john <coxhead.john@e0cf6664-1561-11df-8e3c-378b6dd812f8>
2010-02-18 13:04:49 +0000

    Style cop settings changes

--- src/ScheduMail.Core/RepositoryInterfaces/IRepository.cs
+++ src/ScheduMail.Core/RepositoryInterfaces/IRepository.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 
 namespace ScheduMail.Core.RepositoryInterfaces
 {
+    /// <summary>
+    /// Provides a standard interface for CRUD operations
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="IdT">The type of the d T.</typeparam>
     public interface ICrudRepository<T, IdT>
     {
         IQueryable<T> List { get; }
         T GetById(IdT id);
         T Save(T entity);
         T Update(T entity, T originalEntity);
-        T Update(T entity, T originalEntity, bool attach);
         void Delete(T entity);
     }
 }
---
+++ src/ScheduMail.EFDal/Dal/EFWebSiteRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScheduMail.Core.RepositoryInterfaces;
+using ScheduMail.DBModel;
+
+namespace ScheduMail.EFDal.Dal
+{
+    /// <summary>
+    /// Web site repository crud operations
+    /// </summary>
+    public class EFWebSiteRepository : IWebSiteRepository
+    {
+        #region ICrudRepository<WebSite,int> Members
+
+        /// <summary>
+        /// Gets the list.
+        /// </summary>
+        /// <value>The list.</value>
+        public IQueryable<ScheduMail.DBModel.WebSite> List
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Gets the by id.
+        /// </summary>
+        /// <param name="id">The id.</param>
+        /// <returns>Website instance.</returns>
+        public ScheduMail.DBModel.WebSite GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Saves the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns>Saved Web site instance.</returns>
+        public ScheduMail.DBModel.WebSite Save(ScheduMail.DBModel.WebSite entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Updates the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <param name="originalEntity">The original entity.</param>
+        /// <returns>Updated WebSite instance.</returns>
+        public ScheduMail.DBModel.WebSite Update(ScheduMail.DBModel.WebSite entity, ScheduMail.DBModel.WebSite originalEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Deletes the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        public void Delete(ScheduMail.DBModel.WebSite entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
