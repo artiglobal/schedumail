ce2b7b3be32b4f5c50688286211b8cfcebc11329
coxhead.john <coxhead.john@e0cf6664-1561-11df-8e3c-378b6dd812f8>
2010-02-23 12:13:08 +0000

    Added MVC related Sparks project.

---
+++ trunk/src/ScheduMail.WebMvcSpark/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScheduMail.WebMvcSpark.Controllers
+{
+    /// <summary>
+    /// Home Controller.
+    /// </summary>
+    [HandleError]
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Indexes this instance.
+        /// </summary>
+        /// <returns>The view instance.</returns>
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        /// <summary>
+        /// Abouts this instance.
+        /// </summary>
+        /// <returns>The view instance.</returns>
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ trunk/src/ScheduMail.WebMvcSpark/Models/AccountModel.cs
@@ -0,0 +1,536 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ScheduMail.WebMvcSpark.Models
+{
+    #region Models
+    /// <summary>
+    /// Change Password Model.
+    /// </summary>
+    [PropertiesMustMatch("NewPassword", "ConfirmPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+    public class ChangePasswordModel
+    {
+        /// <summary>
+        /// Gets or sets the old password.
+        /// </summary>
+        /// <value>The old password.</value>
+        [Required]
+        [DataType(DataType.Password)]
+        [DisplayName("Current password")]
+        public string OldPassword { get; set; }
+
+        /// <summary>
+        /// Gets or sets the new password.
+        /// </summary>
+        /// <value>The new password.</value>
+        [Required]
+        [ValidatePasswordLength]
+        [DataType(DataType.Password)]
+        [DisplayName("New password")]
+        public string NewPassword { get; set; }
+
+        /// <summary>
+        /// Gets or sets the confirm password.
+        /// </summary>
+        /// <value>The confirm password.</value>
+        [Required]
+        [DataType(DataType.Password)]
+        [DisplayName("Confirm new password")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    /// <summary>
+    /// The Logon Model.
+    /// </summary>
+    public class LogOnModel
+    {
+        /// <summary>
+        /// Gets or sets the name of the user.
+        /// </summary>
+        /// <value>The name of the user.</value>
+        [Required]
+        [DisplayName("User name")]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>The password.</value>
+        [Required]
+        [DataType(DataType.Password)]
+        [DisplayName("Password")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [remember me].
+        /// </summary>
+        /// <value><c>True</c> If [remember me]; otherwise, <c>false</c>.</value>
+        [DisplayName("Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    /// <summary>
+    /// Register Model class.
+    /// </summary>
+    [PropertiesMustMatch("Password", "ConfirmPassword", ErrorMessage = "The password and confirmation password do not match.")]
+    public class RegisterModel
+    {
+        /// <summary>
+        /// Gets or sets the name of the user.
+        /// </summary>
+        /// <value>The name of the user.</value>
+        [Required]
+        [DisplayName("User name")]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>The email.</value>
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [DisplayName("Email address")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>The password.</value>
+        [Required]
+        [ValidatePasswordLength]
+        [DataType(DataType.Password)]
+        [DisplayName("Password")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets the confirm password.
+        /// </summary>
+        /// <value>The confirm password.</value>
+        [Required]
+        [DataType(DataType.Password)]
+        [DisplayName("Confirm password")]
+        public string ConfirmPassword { get; set; }
+    }
+    #endregion
+
+    #region Services
+
+    /// <summary>
+    /// The FormsAuthentication type is sealed and contains static members, so it is difficult to
+    /// unit test code that calls its members. The interface and helper class below demonstrate
+    /// how to create an abstract wrapper around such a type in order to make the AccountController
+    /// code unit testable.
+    /// </summary>
+    public interface IMembershipService
+    {
+        /// <summary>
+        /// Gets the length of the min password.
+        /// </summary>
+        /// <value>The length of the min password.</value>
+        int MinPasswordLength { get; }
+
+        /// <summary>
+        /// Validates the user.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="password">The password.</param>
+        /// <returns>True if valid user.</returns>
+        bool ValidateUser(string userName, string password);
+
+        /// <summary>
+        /// Creates the user.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="password">The password.</param>
+        /// <param name="email">The email.</param>
+        /// <returns>Membership Create Status.</returns>
+        MembershipCreateStatus CreateUser(string userName, string password, string email);
+
+        /// <summary>
+        /// Changes the password.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="oldPassword">The old password.</param>
+        /// <param name="newPassword">The new password.</param>
+        /// <returns>True if successful.</returns>
+        bool ChangePassword(string userName, string oldPassword, string newPassword);
+    }
+
+    /// <summary>
+    /// The Account Membership Service.
+    /// </summary>
+    public class AccountMembershipService : IMembershipService
+    {
+        /// <summary>
+        /// Membership Provider.
+        /// </summary>
+        private readonly MembershipProvider provider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AccountMembershipService"/> class.
+        /// </summary>
+        public AccountMembershipService()
+            : this(null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AccountMembershipService"/> class.
+        /// </summary>
+        /// <param name="provider">The provider.</param>
+        public AccountMembershipService(MembershipProvider provider)
+        {
+            this.provider = provider ?? Membership.Provider;
+        }
+
+        /// <summary>
+        /// Gets the length of the min password.
+        /// </summary>
+        /// <value>The length of the min password.</value>
+        public int MinPasswordLength
+        {
+            get
+            {
+                return this.provider.MinRequiredPasswordLength;
+            }
+        }
+
+        /// <summary>
+        /// Validates the user.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="password">The password.</param>
+        /// <returns>True if valid user.</returns>
+        public bool ValidateUser(string userName, string password)
+        {
+            if (String.IsNullOrEmpty(userName))
+            {
+                throw new ArgumentException("Value cannot be null or empty.", "userName");
+            }
+
+            if (String.IsNullOrEmpty(password))
+            {
+                throw new ArgumentException("Value cannot be null or empty.", "password");
+            }
+
+            return this.provider.ValidateUser(userName, password);
+        }
+
+        /// <summary>
+        /// Creates the user.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="password">The password.</param>
+        /// <param name="email">The email.</param>
+        /// <returns>Membership Create Status.</returns>
+        public MembershipCreateStatus CreateUser(string userName, string password, string email)
+        {
+            if (String.IsNullOrEmpty(userName))
+            {
+                throw new ArgumentException("Value cannot be null or empty.", "userName");
+            }
+
+            if (String.IsNullOrEmpty(password))
+            {
+                throw new ArgumentException("Value cannot be null or empty.", "password");
+            }
+
+            if (String.IsNullOrEmpty(email))
+            {
+                throw new ArgumentException("Value cannot be null or empty.", "email");
+            }
+
+            MembershipCreateStatus status;
+            this.provider.CreateUser(userName, password, email, null, null, true, null, out status);
+            return status;
+        }
+
+        /// <summary>
+        /// Changes the password.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="oldPassword">The old password.</param>
+        /// <param name="newPassword">The new password.</param>
+        /// <returns>True if successful.</returns>
+        public bool ChangePassword(string userName, string oldPassword, string newPassword)
+        {
+            if (String.IsNullOrEmpty(userName))
+            {
+                throw new ArgumentException("Value cannot be null or empty.", "userName");
+            }
+
+            if (String.IsNullOrEmpty(oldPassword))
+            {
+                throw new ArgumentException("Value cannot be null or empty.", "oldPassword");
+            }
+
+            if (String.IsNullOrEmpty(newPassword))
+            {
+                throw new ArgumentException("Value cannot be null or empty.", "newPassword");
+            }
+
+            // The underlying ChangePassword() will throw an exception rather
+            // than return false in certain failure scenarios.
+            try
+            {
+                MembershipUser currentUser = this.provider.GetUser(userName, true /* userIsOnline */);
+                return currentUser.ChangePassword(oldPassword, newPassword);
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+            catch (MembershipPasswordException)
+            {
+                return false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// IForms Authentication Service.
+    /// </summary>
+    public interface IFormsAuthenticationService
+    {
+        /// <summary>
+        /// Signs the in.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="createPersistentCookie">If set to <c>true</c> [create persistent cookie].</param>
+        void SignIn(string userName, bool createPersistentCookie);
+
+        /// <summary>
+        /// Signs the out.
+        /// </summary>
+        void SignOut();
+    }
+
+    /// <summary>
+    /// Forms Authentication Service.
+    /// </summary>
+    public class FormsAuthenticationService : IFormsAuthenticationService
+    {
+        /// <summary>
+        /// Signs the in.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="createPersistentCookie">If set to <c>true</c> [create persistent cookie].</param>
+        public void SignIn(string userName, bool createPersistentCookie)
+        {
+            if (String.IsNullOrEmpty(userName))
+            {
+                throw new ArgumentException("Value cannot be null or empty.", "userName");
+            }
+
+            FormsAuthentication.SetAuthCookie(userName, createPersistentCookie);
+        }
+
+        /// <summary>
+        /// Signs the out.
+        /// </summary>
+        public void SignOut()
+        {
+            FormsAuthentication.SignOut();
+        }
+    }
+    #endregion
+
+    #region Validation
+    /// <summary>
+    /// Account Validation.
+    /// </summary>
+    public static class AccountValidation
+    {
+        /// <summary>
+        /// Errors the code to string.
+        /// </summary>
+        /// <param name="createStatus">The create status.</param>
+        /// <returns>Error code.</returns>
+        public static string ErrorCodeToString(MembershipCreateStatus createStatus)
+        {
+            // See http://go.microsoft.com/fwlink/?LinkID=177550 for
+            // a full list of status codes.
+            switch (createStatus)
+            {
+                case MembershipCreateStatus.DuplicateUserName:
+                    return "Username already exists. Please enter a different user name.";
+
+                case MembershipCreateStatus.DuplicateEmail:
+                    return "A username for that e-mail address already exists. Please enter a different e-mail address.";
+
+                case MembershipCreateStatus.InvalidPassword:
+                    return "The password provided is invalid. Please enter a valid password value.";
+
+                case MembershipCreateStatus.InvalidEmail:
+                    return "The e-mail address provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidAnswer:
+                    return "The password retrieval answer provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidQuestion:
+                    return "The password retrieval question provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidUserName:
+                    return "The user name provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.ProviderError:
+                    return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                case MembershipCreateStatus.UserRejected:
+                    return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                default:
+                    return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+            }
+        }
+    }
+
+    /// <summary>
+    /// Properties Match attribute.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+    public sealed class PropertiesMustMatchAttribute : ValidationAttribute
+    {
+        /// <summary>
+        /// Default error message.
+        /// </summary>
+        private const string DefaultErrorMessage = "'{0}' and '{1}' do not match.";
+
+        /// <summary>
+        /// Type id for new object.
+        /// </summary>
+        private readonly object TypeIdentification = new object();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PropertiesMustMatchAttribute"/> class.
+        /// </summary>
+        /// <param name="originalProperty">The original property.</param>
+        /// <param name="confirmProperty">The confirm property.</param>
+        public PropertiesMustMatchAttribute(string originalProperty, string confirmProperty)
+            : base(DefaultErrorMessage)
+        {
+            this.OriginalProperty = originalProperty;
+            this.ConfirmProperty = confirmProperty;
+        }
+
+        /// <summary>
+        /// Gets the confirm property.
+        /// </summary>
+        /// <value>The confirm property.</value>
+        public string ConfirmProperty
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the original property.
+        /// </summary>
+        /// <value>The original property.</value>
+        public string OriginalProperty
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// When implemented in a derived class, gets a unique identifier for this <see cref="T:System.Attribute"/>.
+        /// </summary>
+        /// <value>Type id value.</value>
+        /// <returns>
+        /// An <see cref="T:System.Object"/> that is a unique identifier for the attribute.
+        /// </returns>
+        public override object TypeId
+        {
+            get
+            {
+                return this.TypeIdentification;
+            }
+        }
+
+        /// <summary>
+        /// Applies formatting to an error message based on the data field where the error occurred.
+        /// </summary>
+        /// <param name="name">The name of the data field where the error occurred.</param>
+        /// <returns>
+        /// An instance of the formatted error message.
+        /// </returns>
+        public override string FormatErrorMessage(string name)
+        {
+            return String.Format(CultureInfo.CurrentUICulture, this.ErrorMessageString, this.OriginalProperty, this.ConfirmProperty);
+        }
+
+        /// <summary>
+        /// Determines whether the specified value of the object is valid.
+        /// </summary>
+        /// <param name="value">The value of the specified validation object on which the <see cref="T:System.ComponentModel.DataAnnotations.ValidationAttribute"/> is declared.</param>
+        /// <returns>
+        /// True if the specified value is valid; otherwise, false.
+        /// </returns>
+        public override bool IsValid(object value)
+        {
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(value);
+            object originalValue = properties.Find(this.OriginalProperty, true /* ignoreCase */).GetValue(value);
+            object confirmValue = properties.Find(this.ConfirmProperty, true /* ignoreCase */).GetValue(value);
+            return Object.Equals(originalValue, confirmValue);
+        }
+    }
+
+    /// <summary>
+    /// Validate password length.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public sealed class ValidatePasswordLengthAttribute : ValidationAttribute
+    {
+        /// <summary>
+        /// Default error message.
+        /// </summary>
+        private const string DefaultErrorMessage = "'{0}' must be at least {1} characters long.";
+
+        /// <summary>
+        /// Min characters.
+        /// </summary>
+        private readonly int MinCharacters = Membership.Provider.MinRequiredPasswordLength;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ValidatePasswordLengthAttribute"/> class.
+        /// </summary>
+        public ValidatePasswordLengthAttribute()
+            : base(DefaultErrorMessage)
+        {
+        }
+
+        /// <summary>
+        /// Applies formatting to an error message based on the data field where the error occurred.
+        /// </summary>
+        /// <param name="name">The name of the data field where the error occurred.</param>
+        /// <returns>
+        /// An instance of the formatted error message.
+        /// </returns>
+        public override string FormatErrorMessage(string name)
+        {
+            return String.Format(CultureInfo.CurrentUICulture, ErrorMessageString, name, this.MinCharacters);
+        }
+
+        /// <summary>
+        /// Determines whether the specified value of the object is valid.
+        /// </summary>
+        /// <param name="value">The value of the specified validation object on which the <see cref="T:System.ComponentModel.DataAnnotations.ValidationAttribute"/> is declared.</param>
+        /// <returns>
+        /// True if the specified value is valid; otherwise, false.
+        /// </returns>
+        public override bool IsValid(object value)
+        {
+            string valueAsString = value as string;
+            return valueAsString != null && valueAsString.Length >= this.MinCharacters;
+        }
+    }
+    #endregion
+}
