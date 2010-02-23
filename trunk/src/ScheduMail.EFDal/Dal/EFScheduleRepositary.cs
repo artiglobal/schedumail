387349498996b58b1ade14ba944cc06678e4b694
salluu <salluu@e0cf6664-1561-11df-8e3c-378b6dd812f8>
2010-02-23 14:52:41 +0000

    added scheudle interface , repositary and the view so court can work on it

---
+++ trunk/src/ScheduMail.EFDal/Dal/EFScheduleRepositary.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScheduMail.Core.RepositoryInterfaces;
+
+namespace ScheduMail.EFDal.Dal
+{
+    /// <summary>
+    /// Web site repository crud operations for Schedule
+    /// </summary>
+    public class EFScheduleRepositary : IScheduleRepository
+    {
+
+        #region IScheduleRepository Members
+
+        /// <summary>
+        /// Adds the Schedule instance into the dataabse
+        /// </summary>
+        /// <param name="schedule"></param>
+        /// <returns></returns>
+        public bool Add(ScheduMail.DBModel.Schedule schedule)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Update the Schedule instance into the dataabse
+        /// </summary>
+        /// <param name="schedule">contains all the values of schedulee</param>
+        /// <returns>true/false as a sucess message</returns>
+        public bool Update(ScheduMail.DBModel.Schedule schedule)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Delete the Schedule instance from the dataabse
+        /// </summary>
+        /// <param name="id">id of the schedule</param>
+        /// <returns>true/false as a sucess message</returns>
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Returnts the object of  Schedule from ID
+        /// </summary>
+        /// <param name="id">id of the schedule to return</param>
+        /// <returns>object of Schedule</returns>
+        public ScheduMail.DBModel.Schedule Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// returns all the instance from database of schedules
+        /// </summary>
+        /// <returns>return as a list</returns>
+        public List<ScheduMail.DBModel.Schedule> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
